9d8865b151e4250caa9d5c8bfd78ff3d184ed09b
Stephen Dodson <smd8802@icloud.com>
2020-07-14 20:33:07 -0400

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Stephen Dodson on 7/14/20.
+//  Copyright © 2020 Stephen Dodson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
